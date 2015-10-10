160497bf40a8675ab75371722dfab56579f07a86
walterbm <walter.beller.morales@gmail.com>
2015-10-10 17:06:35 -0400

    add test option ethereum contract

--- eth_options/contracts/Option.sol
+++ eth_options/contracts/Option.sol
@@ -1,3 +1,36 @@
 contract Option{
-  
+  address buyer;
+  address seller;
+  uint expiration;
+  uint asset;
+  bytes32 error;
+
+  function setAddresses(address registeredSeller, address registeredBuyer){
+    buyer = registeredBuyer;
+    seller = registeredSeller;
+  }
+  function setExpirationTime(uint expirationSeconds){
+    expiration = block.timestamp + expirationSeconds;
+  }
+
+  function saveAsset(uint assetValue){
+    asset = assetValue;
+  }
+
+  function createOption(address registeredSeller, address registeredBuyer, uint expirationSeconds, uint assetValue){
+    saveAsset(assetValue);
+    setAddresses(registeredSeller,registeredBuyer);
+    setExpirationTime(expirationSeconds);
+  }
+
+  function exercise(){
+    if (now > expiration){
+      buyer.send(asset);
+    }
+    else {
+      error = "Option has not expired!";
+    }
+  }
+
+
 }
