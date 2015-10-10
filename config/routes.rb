08a492d445f0aea803e5b6efa9e288c7a7fdd3e2
ryannyunn <ryannyunn@gmail.com>
2015-10-10 15:56:56 -0400

    users can now create contracts and view available options

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :contracts
-
   devise_for :users
+  resources :contracts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
