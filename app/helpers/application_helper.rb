397928d6983533c44d28d8906e59c96c6ca86a72
ryannyunn <ryannyunn@gmail.com>
2015-10-10 20:25:06 -0400

    modified views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,20 @@ module ApplicationHelper
   def app_name
     'OptionsContract'
   end
+  
+  def resource_name
+    :user
+  end
+  
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_class
+    User
+  end
 end
