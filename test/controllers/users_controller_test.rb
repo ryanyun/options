66cacce79418de4a6b1a05f5afde930ea9e73d3a
Chris Jeon <chris0374@gmail.com>
2015-10-11 10:42:58 -0400

    Finishes user show page

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -9,6 +9,15 @@ class UsersControllerTest < ActionController::TestCase
   test 'GET index when signed in' do
     chris = users(:chris)
     sign_in chris
+    get :index
     assert_response :success
+  end
+
+  test 'GET show' do
+    chris = users(:chris)
+    sign_in chris
+    get :show, id: chris.id
+    assert_response :success
+    assert_equal chris, assigns(:chris)
   end
 end
